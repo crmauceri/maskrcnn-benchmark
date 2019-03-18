bbe72323408e1cc3395b6b7ed698d6dac46143bf
Cecilia <cema3961@colorado.edu>
2019-03-18 14:52:15 -0600

    More general line parsing to dataframe

--- tools/visualize_loss.py
+++ tools/visualize_loss.py
@@ -4,38 +4,43 @@ from pandas import DataFrame
 
 def parse_log(file):
     iter_index_pattern = re.compile(('iter: (\d+)'))
-    pattern = re.compile('(\d+[.]\d+)')
+    pattern = re.compile('([^\s]+): (\d+[.]\d+) \((\d+.\d+)\)')
 
-    parse_train = DataFrame(columns=['iter', 'loss', 'loss_objectness', 'loss_rpn_box_reg', 'text_loss',
-                                     'ref_objectness', 'ref_rpn_box_reg'])
-    parse_val = DataFrame(columns=['iter', 'loss', 'loss_objectness', 'loss_rpn_box_reg', 'text_loss',
-                                     'ref_objectness', 'ref_rpn_box_reg'])
+    parse_train = None
+    parse_val = None
 
     i = 0
     with open(file, 'r') as fp:
         for line in fp:
             if "maskrcnn_benchmark.trainer INFO" in line:
                 iter = re.findall(iter_index_pattern, line)
-                nums = re.findall(pattern, line)
+                loss = re.findall(pattern, line)
 
-                if len(iter)>0 and len(nums)==17:
-                    parse_train.loc[i] = list(map(float, [iter[0]] + nums[0:-5:2]))
-                    parse_val.loc[i] = list(map(float, [iter[0]] + nums[1:-5:2]))
+                if len(iter)>0 and len(loss)>0:
+                    train_vec = [float(iter[0])]
+                    val_vec = [float(iter[0])]
+                    header = ['iter']
+
+                    for tup in loss:
+                        header.append(tup[0])
+                        train_vec.append(float(tup[1]))
+                        val_vec.append(float(tup[2]))
+
+                    if parse_train is None:
+                        parse_train = DataFrame(columns=header)
+                        parse_val = DataFrame(columns=header)
+
+                    parse_train.loc[i] = train_vec
+                    parse_val.loc[i] = val_vec
                     i += 1
 
     plt.plot(parse_train.iter, parse_train.loss)
     plt.title("Total Loss")
     plt.show()
 
-    plt.plot(parse_train.iter, parse_train.text_loss)
-    plt.plot(parse_train.iter, parse_train.ref_rpn_box_reg)
-    plt.plot(parse_train.iter, parse_train.ref_objectness)
-    plt.plot(parse_train.iter, parse_train.loss_rpn_box_reg)
-    plt.plot(parse_train.iter, parse_train.loss_objectness)
-
-    plt.legend(['Text Loss', 'RefExp Loss', 'RefExp Objectness', 'Segmentation Loss', "Segmentation Objectness"], loc='upper left')
-
+    parse_train.plot(x='iter', y=parse_train.columns[2:])
     plt.show()
+
 
     print("Done")
 
