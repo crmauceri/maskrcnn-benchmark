5f26c493d314132d7c95a879ee2ec9b2aabb190d
Cecilia <cema3961@colorado.edu>
2019-03-19 16:31:17 -0600

    Changes to test_net.py  broke everything

--- tools/test_net.py
+++ tools/test_net.py
@@ -19,46 +19,43 @@ from maskrcnn_benchmark.utils.miscellaneous import mkdir
 
 torch.multiprocessing.set_sharing_strategy('file_system')
 
-def test(cfg=None):
+def main():
     save_dir = ""
     logger = setup_logger("maskrcnn_benchmark", save_dir, get_rank())
 
-    if cfg is None:
-        parser = argparse.ArgumentParser(description="PyTorch Object Detection Inference")
-        parser.add_argument(
-            "--config-file",
-            default="/private/home/fmassa/github/detectron.pytorch_v2/configs/e2e_faster_rcnn_R_50_C4_1x_caffe2.yaml",
-            metavar="FILE",
-            help="path to config file",
+    parser = argparse.ArgumentParser(description="PyTorch Object Detection Inference")
+    parser.add_argument(
+        "--config-file",
+        default="/private/home/fmassa/github/detectron.pytorch_v2/configs/e2e_faster_rcnn_R_50_C4_1x_caffe2.yaml",
+        metavar="FILE",
+        help="path to config file",
+    )
+    parser.add_argument("--local_rank", type=int, default=0)
+    parser.add_argument(
+        "opts",
+        help="Modify config options using the command-line",
+        default=None,
+        nargs=argparse.REMAINDER,
+    )
+
+    args = parser.parse_args()
+
+    num_gpus = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
+    distributed = num_gpus > 1
+
+    if distributed:
+        torch.cuda.set_device(args.local_rank)
+        torch.distributed.init_process_group(
+            backend="nccl", init_method="env://"
         )
-        parser.add_argument("--local_rank", type=int, default=0)
-        parser.add_argument(
-            "opts",
-            help="Modify config options using the command-line",
-            default=None,
-            nargs=argparse.REMAINDER,
-        )
+        synchronize()
 
-        args = parser.parse_args()
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
 
-        num_gpus = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
-        distributed = num_gpus > 1
-
-        if distributed:
-            torch.cuda.set_device(args.local_rank)
-            torch.distributed.init_process_group(
-                backend="nccl", init_method="env://"
-            )
-            synchronize()
-
-        cfg.merge_from_file(args.config_file)
-        cfg.merge_from_list(args.opts)
-        cfg.freeze()
-
-        logger.info("Using {} GPUs".format(num_gpus))
-        logger.info(cfg)
-    else:
-        distributed = False
+    logger.info("Using {} GPUs".format(num_gpus))
+    logger.info(cfg)
 
     logger.info("Collecting env info (might take some time)")
     logger.info("\n" + collect_env_info())
@@ -99,4 +96,4 @@ def test(cfg=None):
 
 
 if __name__ == "__main__":
-    test()
+    main()
