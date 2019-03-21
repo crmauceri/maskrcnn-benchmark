fd40918724a87041ef0afd8697485119aa7eb478
Cecilia <cema3961@colorado.edu>
2019-03-21 13:45:35 -0600

    Make weights floats

--- maskrcnn_benchmark/config/defaults.py
+++ maskrcnn_benchmark/config/defaults.py
@@ -416,18 +416,18 @@ _C.SOLVER.IMS_PER_BATCH = 4
 
 _C.LOSS_WEIGHTS = CN()
 _C.LOSS_WEIGHTS.USE_IMG_LOSS = False
-_C.LOSS_WEIGHTS.loss_objectness = 1
-_C.LOSS_WEIGHTS.loss_rpn_box_reg = 1
-_C.LOSS_WEIGHTS.loss_classifier = 1
-_C.LOSS_WEIGHTS.loss_box_reg = 1
-_C.LOSS_WEIGHTS.loss_mask = 1
+_C.LOSS_WEIGHTS.loss_objectness = 1.0
+_C.LOSS_WEIGHTS.loss_rpn_box_reg = 1.0
+_C.LOSS_WEIGHTS.loss_classifier = 1.0
+_C.LOSS_WEIGHTS.loss_box_reg = 1.0
+_C.LOSS_WEIGHTS.loss_mask = 1.0
 _C.LOSS_WEIGHTS.USE_TEXT_LOSS = False
 _C.LOSS_WEIGHTS.TEXT_LOSS = 0.001
-_C.LOSS_WEIGHTS.refexp_loss_objectness = 10
-_C.LOSS_WEIGHTS.refexp_loss_rpn_box_reg = 10
-_C.LOSS_WEIGHTS.refexp_loss_classifier = 10
-_C.LOSS_WEIGHTS.refexp_loss_box_reg = 100
-_C.LOSS_WEIGHTS.refexp_loss_mask = 1
+_C.LOSS_WEIGHTS.refexp_loss_objectness = 10.0
+_C.LOSS_WEIGHTS.refexp_loss_rpn_box_reg = 10.0
+_C.LOSS_WEIGHTS.refexp_loss_classifier = 10.0
+_C.LOSS_WEIGHTS.refexp_loss_box_reg = 100.0
+_C.LOSS_WEIGHTS.refexp_loss_mask = 1.0
 
 # ---------------------------------------------------------------------------- #
 # Specific test options
