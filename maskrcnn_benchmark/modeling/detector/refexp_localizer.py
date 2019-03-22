1eaffe6b26854f8ba96ad067877b882c963e79a7
Cecilia <cema3961@colorado.edu>
2019-03-21 22:00:12 -0600

    Re-add old model from commit 0c4ca87e6bd056780f2025d58f750b9eddd9f86d so that we can run saved weights

--- maskrcnn_benchmark/modeling/detector/refexp_localizer.py
+++ maskrcnn_benchmark/modeling/detector/refexp_localizer.py
@@ -285,3 +285,143 @@ class ReferExpRCNN(nn.Module):
             return losses
 
         return result
+
+    class ReferExpRCNN_Old(DepthRCNN):
+        """
+        Main class for Generalized R-CNN. Currently supports boxes and masks.
+        It consists of three main parts:
+        - backbone
+        = rpn
+        - heads: takes the features + the proposals from the RPN and computes
+            detections / masks from it.
+        """
+
+        def __init__(self, cfg):
+            super(ReferExpRCNN_Old, self).__init__(cfg)
+
+            # Weight losses
+            self.weight_dict = {
+                'loss_objectness': cfg.LOSS_WEIGHTS.loss_objectness,
+                'loss_rpn_box_reg': cfg.LOSS_WEIGHTS.loss_rpn_box_reg,
+                'loss_classifier': cfg.LOSS_WEIGHTS.loss_classifier,
+                'loss_box_reg': cfg.LOSS_WEIGHTS.loss_box_reg,
+                'loss_mask': cfg.LOSS_WEIGHTS.loss_mask
+            }
+
+            # Text Embedding Network
+            self.wordnet = LanguageModel(cfg)
+
+            # Ref Localization Network
+            if self.hha_backbone:
+                self.ref_rpn = build_rpn(cfg, self.image_backbone.out_channels * 2 + 1024)
+                self.ref_roi_heads = build_roi_heads(cfg, self.image_backbone.out_channels * 2 + 1024)
+            else:
+                self.ref_rpn = build_rpn(cfg, self.image_backbone.out_channels + 1024)
+                self.ref_roi_heads = build_roi_heads(cfg, self.image_backbone.out_channels + 1024)
+
+        def instance_prep(self, instance, device, seg_targets):
+            images, HHAs, sentences = instance
+            images, HHAs, seg_targets = super().instance_prep((images, HHAs), device, seg_targets)
+
+            sentences = [s.to(device) for s in sentences]
+
+            ref_targets = []
+            if self.training:
+                if seg_targets is not None:
+                    for ind, s in enumerate(sentences):
+                        s.trim()
+                        ref_targets.extend(s.get_field('ann_target'))
+                else:
+                    ref_targets = None
+
+                ref_targets = [t.to(device) for t in ref_targets]
+
+            return images, HHAs, sentences, seg_targets, ref_targets
+
+        def predictions_forward(self, image_list, features, targets):
+            proposals, proposal_losses = self.ref_rpn(image_list, features, targets)
+
+            if self.ref_roi_heads:
+                x, result, detector_losses = self.ref_roi_heads(features, proposals, targets)
+            else:
+                # RPN-only models don't have roi_heads
+                result = proposals
+                detector_losses = {}
+
+            losses = {}
+            if self.training:
+                losses.update(detector_losses)
+                losses.update(proposal_losses)
+
+                # Change key names
+                keys = losses.keys()
+                key_dict = dict(zip(keys, ['refexp_' + k for k in keys]))
+
+                losses = {key_dict[key]: value * self.weight_dict[key] for (key, value) in losses.items()}
+
+            return result, losses
+
+        def forward(self, instance, device, targets=None):
+            """
+            Arguments:
+                images (list[Tensor] or ImageList): images to be processed
+                targets (list[BoxList]): ground-truth boxes present in the image (optional)
+            Returns:
+                result (list[BoxList] or dict[Tensor]): the output from the model.
+                    During training, it returns a dict[Tensor] which contains the losses.
+                    During testing, it returns list[BoxList] contains additional fields
+                    like `scores`, `labels` and `mask` (for Mask R-CNN models).
+            """
+
+            if self.training and targets is None:
+                raise ValueError("In training mode, targets should be passed")
+
+            image_list, HHA_list, sentences, seg_targets, ref_targets = self.instance_prep(instance, device, targets)
+
+            # Calculate image features
+            image_features = self.features_forward(image_list, HHA_list)
+
+            # Calculate text features
+            sentence_batch = to_tensor_list(sentences)
+            self.wordnet.clear_gradients(batch_size=len(sentence_batch))
+            sentence_targets = sentence_batch.get_target()
+
+            text_prediction = self.wordnet(sentence_batch)
+            text_loss = self.wordnet.loss_function(text_prediction, sentence_targets)
+            text_features = self.wordnet.hidden[0]
+            text_shape = text_features.shape
+
+            full_feature = [0] * len(image_features)
+            image_sizes = []
+            for i, feature in enumerate(image_features):
+                image_shape = feature.shape
+                full_shape = torch.Size((text_shape[1], image_shape[1] + text_shape[2], image_shape[2], image_shape[3]))
+                f = torch.zeros(full_shape, device=device)
+
+                f[:, image_shape[1]:, :, :] = text_features.reshape((text_shape[1], text_shape[2], 1, 1)).repeat(1, 1,
+                                                                                                                 image_shape[
+                                                                                                                     2],
+                                                                                                                 image_shape[
+                                                                                                                     3])
+                image_mask = [len(s) for s in sentences]
+                for j, repeats in enumerate(image_mask):
+                    f[sum(image_mask[:j]):sum(image_mask[:j + 1]), :image_shape[1], :, :] = feature[j, :, :, :].repeat(
+                        repeats, 1, 1, 1)
+                    image_sizes.extend(list(itertools.repeat(image_list.image_sizes[j], repeats)))
+                full_feature[i] = f
+
+            ## Losses and predictions
+            result, ref_exp_loss = self.predictions_forward(image_sizes, full_feature, ref_targets)
+
+            if self.training:
+                # Normal instance segmentation
+                losses = super().predictions_forward(image_list, image_features, seg_targets)[1]
+
+                # Language model
+                losses['text_loss'] = text_loss
+
+                # Referring Expression Localization
+                losses.update(ref_exp_loss)
+                return losses
+
+            return result
