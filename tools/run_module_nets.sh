9783c5f8c5fd0750d6ad87059b76e7fef173eb92
Cecilia <cema3961@colorado.edu>
2019-04-29 15:54:09 -0600

    I'll run four of the six experiments

--- tools/run_module_nets.sh
+++ tools/run_module_nets.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+# Language Models
 CUDA_VISIBLE_DEVICES=0 python tools/train_net.py --config-file configs/LSTM_classification_experiment.yaml SOLVER.IMS_PER_BATCH 4 MODEL.DEVICE cpu
 CUDA_VISIBLE_DEVICES=0 python tools/train_net.py --config-file configs/LSTM_language_experiment.yaml SOLVER.IMS_PER_BATCH 4 MODEL.DEVICE cpu
-CUDA_VISIBLE_DEVICES=0 python tools/train_net.py --config-file configs/refcoco_segmentation.yaml SOLVER.IMS_PER_BATCH 4 MODEL.DEVICE cpu
-CUDA_VISIBLE_DEVICES=0 python tools/train_net.py --config-file configs/refcoco_depth_segmentation.yaml SOLVER.IMS_PER_BATCH 4 MODEL.DEVICE cpu
+
+# Segmentation Models
+# CUDA_VISIBLE_DEVICES=0 python tools/train_net.py --config-file configs/refcoco_segmentation.yaml SOLVER.IMS_PER_BATCH 4 MODEL.DEVICE cpu
+# CUDA_VISIBLE_DEVICES=0 python tools/train_net.py --config-file configs/refcoco_depth_segmentation.yaml SOLVER.IMS_PER_BATCH 4 MODEL.DEVICE cpu
+
+# Referring Expression Models
+CUDA_VISIBLE_DEVICES=0 python tools/train_net.py --config-file configs/refcocog_refexprcnn_classification_model.yaml SOLVER.IMS_PER_BATCH 4 #MODEL.DEVICE cpu
+CUDA_VISIBLE_DEVICES=0 python tools/train_net.py --config-file configs/refcocog_refexprcnn_language_model.yaml SOLVER.IMS_PER_BATCH 4 #MODEL.DEVICE cpu
